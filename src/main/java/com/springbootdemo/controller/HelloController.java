97a4bc9715e87bc733ef844a90bb7e7435eea44e
hanchaozheng <hanchaozheng@yahoo.com>
2019-03-06 16:41:00 +0800

    springboot demo

--- src/main/java/com/springbootdemo/controller/HelloController.java
+++ src/main/java/com/springbootdemo/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.springbootdemo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
@@ -9,5 +13,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello springboot";
+    }
+    @RequestMapping(value = {"/requestDemo"},method = {RequestMethod.GET})
+    public String requestDemo(HttpServletRequest request, Model model){
+        String id = request.getParameter("id");
+        return "您输入的id是:"+id;
     }
 }
