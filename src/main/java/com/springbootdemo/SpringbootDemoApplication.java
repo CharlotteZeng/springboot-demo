32000770d6bdb1e481c4e3806a38080d2f035df6
hanchaozheng <hanchaozheng@yahoo.com>
2019-03-06 16:41:27 +0800

    spring demo

--- src/main/java/com/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/springbootdemo/SpringbootDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 /**
+ * springboot启动类
  * springboot 只能扫描此类下面的路径吓得controllere
  * 所以如果controller不在application类的下面 可以加上
  * @ComponentScan(basePackages = {"com.springbootdemo.*"}) 来指定要扫描的包
