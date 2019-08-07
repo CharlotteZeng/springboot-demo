792c14d6f67a1b26e8ce24e1d069b68153d74935
charlotte <hanchaozheng@yahoo.com>
2019-08-07 08:37:53 +0800

    练习代码 lock的等待与唤醒

---
+++ src/test/java/com/springbootdemo/test/ProdConsumer_TraditionDemo.java
@@ -0,0 +1,8 @@
+package com.springbootdemo.test;
+
+public class ProdConsumer_TraditionDemo {
+    public static void main(String[] args){
+        ShareData shareData = new ShareData();
+
+    }
+}
