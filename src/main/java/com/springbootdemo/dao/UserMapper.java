a4ad39ab71f14de9bb3f27d2d111c53856e7078d
charlotte <hanchaozheng@yahoo.com>
2022-03-17 15:16:36 +0800

    用户登陆 与 注册持久层代码

--- src/main/java/com/springbootdemo/dao/UserMapper.java
+++ src/main/java/com/springbootdemo/dao/UserMapper.java
@@ -1,10 +1,17 @@
 package com.springbootdemo.dao;
 
 import com.springbootdemo.entity.User;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    public void insert(User user);
+    @SelectKey(keyProperty = "user.uid",before = true,statement = "select MD5(replace(uuid(), '-', ''))", resultType = String.class)
+    @Options(useGeneratedKeys=true, keyProperty="user.uid")
+    @Insert("insert into user(uname,password) VALUES(user.uname,user.password)" )
+    public int insertByUser(@Param("user") User user);
+    @Select("select * from user where uname = #{user.uname} and password = #{user.password}")
+    public List<User> findByUser(@Param("user")User user);
     public void update(User user);
 }
